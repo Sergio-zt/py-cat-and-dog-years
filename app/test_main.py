d75b8f386c1c7018f3fb4b9cadadc4a91c7b74b1
Sergio-zt <fop.gso@gmail.com>
2026-04-08 14:52:26 -0400

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,41 @@
 from app.main import get_human_age
+import pytest
 
+ 
+class TestCheckAge:
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, expected_res",
+        [
+            (14, 14, [0, 0]),
+            (15, 15, [1, 1]),
+            (23, 23, [1, 1]),
+            (24, 24, [2, 2]),
+            (27, 28, [2, 2]),
+            (28, 29, [3, 3]),
+            (0, 0, [0, 0]),
+            (-20, -30, [0, 0]),
+            (100, 100, [21, 17])
+        ]
+    )
+    def test_check_correct_return(
+        self,
+        cat_age,
+        dog_age,
+        expected_res
+    ) -> None:
+        assert get_human_age(cat_age, dog_age) == expected_res
 
-def test_check_if_14_cat_dog_yers_return_0() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_check_if_15_cat_dog_yers_return_1() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_check_if_23_cat_dog_yers_return_1() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_check_if_24_cat_dog_yers_return_2() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_check_if_27_28_cat_dog_yers_return_2() -> None:
-    assert get_human_age(27, 28) == [2, 2]
-
-
-def test_check_if_28_29_cat_dog_yers_return_2() -> None:
-    assert get_human_age(28, 29) == [3, 3]
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, expected_error",
+        [
+            ("0", "14", TypeError)
+        ]
+    )
+    def test_get_errors(
+        self,
+        cat_age,
+        dog_age,
+        expected_error
+    ) -> None:
+        with pytest.raises(expected_error):
+            get_human_age(cat_age, dog_age)
